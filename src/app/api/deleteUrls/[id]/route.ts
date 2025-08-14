21f3299fd0eae4ef0d050c6dc21971dfea053c1a
carlos ortega <sr.carlosortega@gmail.com>
2025-08-13 23:42:39 -0300

    Refactor deleteUrls API to use dynamic route
    Moved the deleteUrls API logic from a static route to a dynamic [id] route for better RESTful design. The DELETE handler now accepts the URL id as a route parameter, improving clarity and usability.

---
+++ src/app/api/deleteUrls/[id]/route.ts
@@ -0,0 +1,16 @@
+import prisma from "../../../../../prisma/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function DELETE(
+    request: Request,
+    { params }: { params: { id: string } }
+) {
+    const { id } = params;
+  await prisma.url.deleteMany({
+    where: { id: id },
+  });
+  return NextResponse.json(
+    { message: "URL deleted successfully" },
+    { status: 200 }
+  );
+}
